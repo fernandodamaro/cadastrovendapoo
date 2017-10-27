11940014176d50de8a9f0459245f01e61ca7ce90
FERNANDO DAMARO CORREIA DAS NEVES <32998949860@jpmorgan.sp>
2017-10-27 18:19:56 -0200

    criacao do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadastrovendapoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
